eace74879e7e1bf7d9b13046d1e5b95b6c391ef3
Mohsin <44529081+MohsinEjaz21@users.noreply.github.com>
2022-06-14 01:28:49 +0500

    Testing children props [R9-props]

--- src/pages/Greet.tsx
+++ src/pages/Greet.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export const GreetDump = (props) => {
-  const { name } = props;
+  const { name, children } = props;
   return (
     <>
-      <h1 >
+      <h1>
         {name && `Hi ${name}`}
         {!name && 'Hello World'}
       </h1>
+      {children}
     </>
   )
 }
@@ -15,7 +16,9 @@ export const GreetDump = (props) => {
 export const Greet = () => {
   return (
     <>
-      <GreetDump name={'Mohsin Ejaz'} />
+      <GreetDump name={'Mohsin Ejaz'} >
+        <p>This is the testing paragraph</p>
+      </GreetDump>
       <GreetDump name={'Sajjad'} />
       <GreetDump />
     </>
