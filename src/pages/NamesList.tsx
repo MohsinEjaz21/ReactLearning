fbe3763864e72ed554c92ced2099ecd6a613bf83
Mohsin <44529081+MohsinEjaz21@users.noreply.github.com>
2022-06-14 14:41:50 +0500

    R19-IndexAsKeyAntipattern done

--- src/pages/NamesList.tsx
+++ src/pages/NamesList.tsx
@@ -10,6 +10,9 @@ export const NamesList = () => {
   ]
 
   const personsJSX = persons.map((person: IApp.Person, index) => {
+    // here index is used as key which is not recommended
+    // as it will be changed every time when the list is updated
+    // so we should use unique key so that it will be easy for REACT to track the changes
     return <li key={index}>{<Person {...person} />}</li>
   });
 
