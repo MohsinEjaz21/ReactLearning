925a6dd54e5d6acbfc5111d283fd4d0dc74bea19
Mohsin <44529081+MohsinEjaz21@users.noreply.github.com>
2022-07-04 11:11:21 +0500

    Working on query

--- src/components/widgets/FilterChips.tsx
+++ src/components/widgets/FilterChips.tsx
@@ -5,21 +5,25 @@ export function FilterChips({ props: { tags, setTags, applyFilters } }) {
 
   function makeQuery() {
     const duplicateCols = findduplicateColumns();
+    let param: { key: string, value: string }[] = [];
     let query1 = "", query2 = "";
-    duplicateCols.forEach(col => { query2 += createQueryForDuplicates([col]) })
-    query1 = createQueryForNonDuplicates(duplicateCols) || "";
+    duplicateCols.forEach(col => { query2 += createQueryForDuplicates([col], param) })
+    query1 = createQueryForNonDuplicates(duplicateCols, param) || "";
     const query = query1 + query2;
-    applyFilters(query);
+    applyFilters({ query, param });
   }
 
-  function createQueryForNonDuplicates(duplicateCols) {
+  function createQueryForNonDuplicates(duplicateCols, param) {
     let query = ""
     const nonDuplicates = tags.filter(({ column }) => !duplicateCols.includes(column));
     nonDuplicates.map((tag, index) => {
       if (index !== 0 && index !== nonDuplicates.length) {
         query += " AND ";
       }
-      query += `${tag.column} ${tag.operator} ${tag.value}`;
+      let tempColumn = tag.column.replace(".", "_");
+      let key = `${tempColumn}_${index}`;
+      query += `${tag.column} ${tag.operator} :${key}`;
+      param.push({ key: key, value: tag.value })
     });
     if (!!query.length) {
       query = ` AND (${query})`;
@@ -28,14 +32,18 @@ export function FilterChips({ props: { tags, setTags, applyFilters } }) {
   }
 
 
-  function createQueryForDuplicates(duplicateCol) {
+  function createQueryForDuplicates(duplicateCol, param) {
     let query = ""
     const duplicates = tags.filter(({ column }) => duplicateCol == column);
     duplicates.map((tag, index) => {
       if (index !== 0 && index !== duplicates.length) {
         query += " OR ";
       }
-      query += ` ${tag.column} ${tag.operator} ${tag.value} `;
+      let tempColumn = tag.column.replace(".", "_");
+      let key = `${tempColumn}_${index}`;
+      query += `${tag.column} ${tag.operator} :${key}`;
+      param.push({ key: key, value: tag.value })
+
     });
     if (!!query.length) {
       query = ` AND (${query}) `;
