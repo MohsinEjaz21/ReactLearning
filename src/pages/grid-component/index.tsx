d49a6ecf5be81fb3bb581a201ab8ce8638136cb8
Mohsin <mejaz.bese21@gmail.com>
2022-08-04 00:15:01 +0500

    cleaning code

--- src/pages/grid-component/index.tsx
+++ src/pages/grid-component/index.tsx
@@ -1,8 +1,21 @@
+import '@styles/base.css'
+import '@styles/grid.css'
 
 function index() {
   return (
-    <div>
-      This is Grid Component
+    <div className="grid">
+      <h1 className="card">1</h1>
+      <h1 className="card">2</h1>
+      <h1 className="card">3</h1>
+      <h1 className="card">4</h1>
+      <h1 className="card">5</h1>
+      <h1 className="card">6</h1>
+      <h1 className="card">7</h1>
+      <h1 className="card">8</h1>
+      <h1 className="card">9</h1>
+      <h1 className="card">10</h1>
+      <h1 className="card">11</h1>
+      <h1 className="card">12</h1>
     </div>
   )
 }
