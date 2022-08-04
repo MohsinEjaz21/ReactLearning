34e96c14b187995b4a5bc41f08d64b4c253b8451
Mohsin <mejaz.bese21@gmail.com>
2022-08-04 17:49:43 +0500

    away-surgery-practice-css

--- src/pages/grid-component/index.tsx
+++ src/pages/grid-component/index.tsx
@@ -1,5 +1,3 @@
-import '@styles/base.scss';
-import '@styles/grid.scss';
 
 function GridComponent() {
   return (
@@ -7,15 +5,6 @@ function GridComponent() {
       <h1 className="card">1</h1>
       <h1 className="card">2</h1>
       <h1 className="card">3</h1>
-      {/* <h1 className="card">4</h1>
-      <h1 className="card">5</h1>
-      <h1 className="card">6</h1>
-      <h1 className="card">7</h1>
-      <h1 className="card">8</h1>
-      <h1 className="card">9</h1>
-      <h1 className="card">10</h1>
-      <h1 className="card">11</h1>
-      <h1 className="card">12</h1> */}
     </div>
   )
 }
