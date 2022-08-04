743f1829b061cd1fed8404de3364575eddc58af7
Mohsin <mejaz.bese21@gmail.com>
2022-08-04 10:09:13 +0500

    css-simple-grid

--- src/pages/grid-component/index.tsx
+++ src/pages/grid-component/index.tsx
@@ -7,7 +7,7 @@ function index() {
       <h1 className="card">1</h1>
       <h1 className="card">2</h1>
       <h1 className="card">3</h1>
-      <h1 className="card">4</h1>
+      {/* <h1 className="card">4</h1>
       <h1 className="card">5</h1>
       <h1 className="card">6</h1>
       <h1 className="card">7</h1>
@@ -15,7 +15,7 @@ function index() {
       <h1 className="card">9</h1>
       <h1 className="card">10</h1>
       <h1 className="card">11</h1>
-      <h1 className="card">12</h1>
+      <h1 className="card">12</h1> */}
     </div>
   )
 }
