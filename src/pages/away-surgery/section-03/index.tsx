d12fd232f3992eb4d5028f8ee801182674292ce9
Mohsin <mejaz.bese21@gmail.com>
2022-08-06 11:32:45 +0500

    section03-finishing

--- src/pages/away-surgery/section-03/index.tsx
+++ src/pages/away-surgery/section-03/index.tsx
@@ -1,48 +1,33 @@
-/* This source code is exported from pxCode, you can get more document from https://www.pxcode.io */
-import cn from 'classnames';
-import styled from 'styled-components';
-import styles from './section-03.module.scss';
-
+import './section-03.scss';
 export function SectionO3(props) {
-  const image: any = {
-    '--src': `url(https\:\/\/storage\.googleapis\.com\/pxcode-pro\/62ed0331a8ac320014e22cf8\%2Fassets\%2F9fd18e66ae19bf04e2926efc5296a7e7\.png\?GoogleAccessId\=gcs-app\%40pxcode\.iam\.gserviceaccount\.com\&Expires\=2290420035\&Signature\=P\%2F2N6BTq\%2BPQwTkgAWtZE1a0OFjQmkK3w8YeYPC\%2BzKE4uObulj2uPS9a7ghjlcD0QZ21GYeeEpFvYNwWsuWMv7RZW9TvJL6IAre8NHVMOYwW4B5kH\%2F7KnZ\%2Fpc1HRYyEPfvZgdHBoIZ6Cbedkbj5IARhXk8zCGRcL\%2BwMl3oyn89Z2LJ8gH7HMQLSbkhXpIDMBSnGxn1MPsdDPwk3ksDzt\%2BCfxm1bKmJpUqIjxhoC3GgSOfYwSuX66vnc8Kdhz39BoP0sCbdQe1W8\%2BYrGGLEvM2hIpVIS2VOdlw2tKBk\%2BskJcoNc97\%2BQnVv3xb1zqv8nKXnEfwgKPsFBIRPX5Iuplfs1A\%3D\%3D)`,
-  }
 
   return (
-    <Section>
-      <div className={`${cn(styles.block, styles.block_layout, styles.full_bleed)}`}>
 
-        <div className={cn(styles.block2, styles.block2_layout)}>
-          <h2 className={cn(styles.medium_title, styles.medium_title_layout)}>{'Clineca Consultaion'}</h2>
-          <h1 className={cn(styles.hero_title_box, styles.hero_title_box_layout)}>
-            <pre className={styles.hero_title}>
-              {'We’re open and welcoming  patients! Let us make an appointment for your online consultation.'}
-            </pre>
-          </h1>
-
-          <div className={cn(styles.block3, styles.block3_layout)}>
-            <h5 className={cn(styles.highlights, styles.highlights_layout)}>{'Book Online Consultation'}</h5>
+    <div className="screen03 screen03-block layout full-bleed">
+      <div className="screen03-flex layout">
+        <div className="screen03-flex-item">
+          <div className="screen03-group layout screen03-full-bleed">
+            <img src="../../../../assets/images/img20.svg" alt="" className="screen03-block4 layout" />
+            <div className="screen03-box1 layout" />
+            <div className="screen03-image layout" />
           </div>
         </div>
-
-        <div className={cn(styles.group, styles.group_layout)}>
-          <div className={cn(styles.box1, styles.box1_layout)} />
-          <div
-            style={image}
-            className={cn(styles.image, styles.image_layout)}
-          />
-          <img
-            src={'assets/images/img20.svg'}
-            className={cn(styles.block4, styles.block4_layout)}
-          />
+        <div className="screen03-flex-spacer" />
+        <div className="screen03-flex-item1">
+          <div className="screen03-block2 layout">
+            <h2 className="screen03-medium-title layout">Clineca Consultaion</h2>
+            <h1 className="screen03-hero-title-box layout">
+              <pre className="screen03-hero-title">We’re open and welcoming{"  "}patients! Let us make an appointment for your online consultation.</pre>
+            </h1>
+            <div className="screen03-block3 layout">
+              <h5 className="screen03-highlights layout">Book Online Consultation</h5>
+            </div>
+          </div>
         </div>
       </div>
-    </Section>
+    </div>
+
 
   );
 }
 
-const Section = styled.div`
-  display: flex;
-  justify-content: center;
-`
