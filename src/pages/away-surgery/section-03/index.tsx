905745bdda1d5d72aaf269f8b5a23693928c1d70
Mohsin <mejaz.bese21@gmail.com>
2022-08-06 18:13:38 +0500

    finalized section 03

--- src/pages/away-surgery/section-03/index.tsx
+++ src/pages/away-surgery/section-03/index.tsx
@@ -7,12 +7,12 @@ export function SectionO3(props) {
     <div className="section-03">
       <div className="container">
         <div className="left-section">
-          <div className="image1" />
-          <div className="image2" />
-          <div className="image3" />
+          <div className="content">
+            <div className="image1" />
+          </div>
         </div>
         <div className="right-section">
-          <div className="right-container">
+          <div className="content">
             <h1 className="text1">Clineca Consultaion</h1>
             <h2 className="text2">We’re open and welcoming  patients! Let us make an appointment for your online consultation.</h2>
             <div className="w-btn">Book Online Consultation</div>
@@ -24,6 +24,3 @@ export function SectionO3(props) {
   );
 }
 
-
-// src="../../../../assets/images/img20.svg
-// background-image: url("../../../../assets/images/image_21.png")
