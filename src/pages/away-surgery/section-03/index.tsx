6af7b7055b66ae28c7dbb9ee75165d7162d054ce
Mohsin <mejaz.bese21@gmail.com>
2022-08-06 14:45:22 +0500

    contd

--- src/pages/away-surgery/section-03/index.tsx
+++ src/pages/away-surgery/section-03/index.tsx
@@ -1,33 +1,29 @@
-import './section-03.scss';
+import '@styles/scss-global.scss';
+
 export function SectionO3(props) {
 
   return (
 
-    <div className="screen03 screen03-block layout full-bleed">
-      <div className="screen03-flex layout">
-        <div className="screen03-flex-item">
-          <div className="screen03-group layout screen03-full-bleed">
-            <img src="../../../../assets/images/img20.svg" alt="" className="screen03-block4 layout" />
-            <div className="screen03-box1 layout" />
-            <div className="screen03-image layout" />
-          </div>
+    <div className="section-03">
+      <div className="container">
+        <div className="left-section">
+          <div className="image1" />
+          <div className="image2" />
+          <div className="image3" />
         </div>
-        <div className="screen03-flex-spacer" />
-        <div className="screen03-flex-item1">
-          <div className="screen03-block2 layout">
-            <h2 className="screen03-medium-title layout">Clineca Consultaion</h2>
-            <h1 className="screen03-hero-title-box layout">
-              <pre className="screen03-hero-title">We’re open and welcoming{"  "}patients! Let us make an appointment for your online consultation.</pre>
-            </h1>
-            <div className="screen03-block3 layout">
-              <h5 className="screen03-highlights layout">Book Online Consultation</h5>
-            </div>
+        <div className="right-section">
+          <div className="right-container">
+            <h1 className="text1">Clineca Consultaion</h1>
+            <h2 className="text2">We’re open and welcoming  patients! Let us make an appointment for your online consultation.</h2>
+            <div className="w-btn">Book Online Consultation</div>
           </div>
         </div>
       </div>
     </div>
 
-
   );
 }
 
+
+// src="../../../../assets/images/img20.svg
+// background-image: url("../../../../assets/images/image_21.png")
