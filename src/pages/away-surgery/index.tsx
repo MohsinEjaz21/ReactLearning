d7e8946449a397a44fe2baead3fdc7ef7ef497d4
Mohsin <mejaz.bese21@gmail.com>
2022-08-07 08:40:29 +0500

    Update index.tsx

--- src/pages/away-surgery/index.tsx
+++ src/pages/away-surgery/index.tsx
@@ -3,14 +3,16 @@ import { SectionO2 } from "./section-02";
 import { SectionO3 } from "./section-03";
 import { SectionO4 } from "./section-04";
 import { SectionO5 } from "./section-05";
+import { SectionO6 } from "./section-06";
 
 export function AwaySurgery(props) {
   return (
     <>
-      <SectionO1 />
+      <SectionO6 />
+      <SectionO5 />
       <SectionO4 />
       <SectionO2 />
-      <SectionO5 />
+      <SectionO1 />
       <SectionO3 />
     </>
   );
