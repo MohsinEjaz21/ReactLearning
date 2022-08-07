e37d530e4da068c17e47e3b50a3b6ba1afd3b809
Mohsin <mejaz.bese21@gmail.com>
2022-08-07 05:27:27 +0500

    dynamic listing

--- src/pages/away-surgery/section-04/index.tsx
+++ src/pages/away-surgery/section-04/index.tsx
@@ -1,10 +1,30 @@
 import './section-04.scss';
 
 
-export function SectionO4(props) {
-  const icon = "../../../../assets/images/sec04/img-1.svg";
-  const image = "../../../../assets/images/sec04/img-2.png";
 
+
+
+function ForeachElement(props) {
+  const icon = "../../../../assets/images/sec04/img-1.svg";
+  return (
+    <li className="li">
+      <img src={icon} />
+      <p className="text">{props.text}</p>
+    </li>
+  );
+}
+
+const listOfElements = [
+  { text: "Прибиране на пригорещения и пригорещения" },
+  { text: "Прибиране на пригорещения и пригорещения" },
+  { text: "Прибиране на пригорещения и пригорещения" },
+  { text: "Прибиране на пригорещения и пригорещения" }
+]
+
+
+export function SectionO4(props) {
+  const image = "../../../../assets/images/sec04/img-2.png";
+  const text = "Your breasts might be covered with bandages or dressings to reduce swelling and to support the new shape of your breasts.";
   return (
     <div className="section-04">
       <div className="block-1">
@@ -16,24 +36,7 @@ export function SectionO4(props) {
         <div className="content">
           <h1 className="heading">The First Week</h1>
           <ul className="ul">
-            <li className="li">
-              <img src={icon} />
-              <p className="text">
-                Your breasts might be covered with bandages or dressings to reduce swelling and to support the new shape of your breasts.
-              </p>
-            </li>
-            <li className="li">
-              <img src={icon} />
-              <p className="text">
-                Your breasts might be covered with bandages or dressings to reduce swelling and to support the new shape of your breasts.
-              </p>
-            </li>
-            <li className="li">
-              <img src={icon} />
-              <p className="text">
-                Your breasts might be covered with bandages or dressings to reduce swelling and to support the new shape of your breasts.
-              </p>
-            </li>
+            {listOfElements?.map(e => <ForeachElement text={e.text} />)}
           </ul>
         </div>
       </div>
