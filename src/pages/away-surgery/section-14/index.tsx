215eb6f2b0d4ac3273a485a03f35e677f351d784
Mohsin <mejaz.bese21@gmail.com>
2022-08-13 13:14:34 +0500

    away-surgery-restart

---
+++ src/pages/away-surgery/section-14/index.tsx
@@ -0,0 +1,11 @@
+
+
+export function Section14(props) {
+
+  return (
+    <div className="section-14">
+      <h1 className="typetura-headline">Hello World</h1>
+    </div>
+  );
+}
+
--- src/routes/frontend/Routes.tsx
+++ src/routes/frontend/Routes.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Slider from '../../components/slider';
 import { AwaySurgery } from '../../pages/away-surgery';
-import { Section13 } from '../../pages/away-surgery/section-13';
+import { Section14 } from '../../pages/away-surgery/section-14';
 import GridComponent from '../../pages/grid-component';
 
 const AppRoutes = () => {
@@ -10,7 +10,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<GridComponent />} />
         <Route path="/aws" element={<AwaySurgery />} />
-        <Route path="/sec" element={<Section13 />} />
+        <Route path="/sec" element={<Section14 />} />
         <Route path="/temp" element={<Slider />} />
 
       </Routes>
