83e1a5c1a8e45dc9059d4bae77cd81dabb94b252
Mohsin Ejaz <mejaz.bese21@gmail.com>
2023-10-30 14:50:18 +0500

    cleanup

--- src/redux/InventorySlice.tsx
+++ src/redux/InventorySlice.tsx
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const DataGridSlice = createSlice({
-  name: 'DataGridSlice',
+  name: 'InventorySlice',
   initialState: {
     isFilterModalOpen: false,
     isAddModalOpen: false,
